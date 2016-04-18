34279c9009336ed562e81cc871a52e9cf4a058e1
Garmega <garudaomega@gmail.com>
2016-04-17 20:30:28 -0700

    Trying to download data. Not working.

--- android/app/src/main/java/edu/uw/mao1001/todoer/MainActivity.java
+++ android/app/src/main/java/edu/uw/mao1001/todoer/MainActivity.java
@@ -1,18 +1,48 @@
 package edu.uw.mao1001.todoer;
 
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import edu.uw.todoer.provider.TodoListProvider;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TaskListFragment fragment = (TaskListFragment)getSupportFragmentManager().findFragmentByTag("TaskListFragment");
-        if (fragment != null) {
-            fragment = new TaskListFragment();
+
+
+        getContentResolver().insert()
+
+//        TaskListFragment fragment = (TaskListFragment)getSupportFragmentManager().findFragmentByTag("TaskListFragment");
+//        fragment = new TaskListFragment();
+//
+//        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+//        ft.replace(R.id.container, fragment, "TaskListFragment");
+//        ft.commit();
+//    }
+
+        private String buildURI(String city) {
+            Uri.Builder builder = new Uri.Builder();
+            builder.scheme("http")
+                    .authority("api.openweathermap.org")
+                    .appendPath("data")
+                    .appendPath("2.5")
+                    .appendPath("forecast")
+                    .appendQueryParameter("q", city)
+                    .appendQueryParameter("format", "JSON")
+                    .appendQueryParameter("units", "imperial")
+                    .appendQueryParameter("appid", BuildConfig.OPEN_WEATHER_MAP_API_KEY);
+
+            //Log.i(TAG, builder.build().toString());
+            return builder.build().toString();
         }
+
     }
-}
--- android/app/src/main/java/edu/uw/mao1001/todoer/TaskListFragment.java
+++ android/app/src/main/java/edu/uw/mao1001/todoer/TaskListFragment.java
@@ -2,14 +2,26 @@ package edu.uw.mao1001.todoer;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import edu.uw.todoer.provider.TodoListProvider;
 
 /**
  * Created by Nick on 4/17/2016.
  */
 public class TaskListFragment extends Fragment {
+
+    private static final String TAG = "TaskListFragment";
+
+    private ArrayAdapter<String> adapter;
 
     public TaskListFragment() {
         //Required blank constructor
@@ -18,6 +30,44 @@ public class TaskListFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_task_list, container, false);
+
+        Log.v(TAG, "onCreateView");
+
+        //controller
+        adapter = new ArrayAdapter<String>(this.getActivity(),
+                R.layout.list_item, R.id.txtItem, new ArrayList<String>());
+
+        ListView listView = (ListView)rootView.findViewById(R.id.taskListView);
+        listView.setAdapter(adapter);
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Movie movie = (Movie)parent.getItemAtPosition(position);
+//                Log.v(TAG, "You clicked on: "+movie);
+//
+//                //tell the activity to do stuff!
+//                //((OnMovieSelectedListener)getActivity()).movieSelected(movie);
+//                callback.movieSelected(movie);
+//
+//            }
+//        });
+//        Log.v(TAG, "In create bundle: "+savedInstanceState);
+//        if(savedInstanceState != null && savedInstanceState.getString("searchTerm") != null) {
+//            fetchData(savedInstanceState.getString("searchTerm"));
+//        }
+
+
+
+        adapter.add("Data1");
+        adapter.add("Data2");
+        adapter.add("Data3");
+
+        return rootView;
+    }
+
+
+    public void fetchData() {
+
     }
 }
