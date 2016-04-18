4f9767855774d8cbd786902db76a1b88db364a04
C Nicholas Mao <mao@tacktechnologies.com>
2016-04-17 18:07:40 -0700

    created android project

---
+++ android/app/src/main/java/edu/uw/mao1001/todoer/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.uw.mao1001.todoer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
